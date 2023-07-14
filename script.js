60edbb4a3066e1b97290996f7433becd7771c4f9
Manish Chaudhari <chaudharimanish07@gmail.com>
2023-07-13 23:05:18 -0400

    created a seperate Human class and an Alien class

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
 
-//global Vars
-let shipCount=0;
+
+
 
 
 //Classes
 
-class Spaceship {
+
+
+class AlienSpaceship {
     constructor (hull=0, firepower=0, accuracy=0){
         this.hull = randomHull(3,6);
         this.firepower = randomFirePower(2,4);
@@ -14,78 +16,130 @@ class Spaceship {
 
     }
 
+    //Randomize functions
+    // hull - between 3 and 6
+    randomHull(min,max){
+        let randNum = Math.random()*(max-min);
 
+        if (randNum < min){
+        randNum = Math.round(randNum)+min;
+        }
+    return  randNum;
+    }
+
+
+    // firepower - between 2 and 4
+    randomFirePower(min,max){
+
+        let randNum = Math.random()*(max-min);
+
+        if (randNum < min){
+        randNum = Math.round(randNum)+min;
+        }
+    return  randNum;
+    }
+
+    // accuracy - between .6and .8
+
+    randomAccuracy(min,max){
+
+        let randNum = (Math.random()*max);
+        randNum = ((randNum%Math.random())*Math.random());
+        randNum = (Math.random()*randNum);
+
+        if (randNum < min){
+            randNum +=min;
+        }
+        if (randNum >max){
+            randNum = randNum%max;
+        }
+
+    return  parseFloat(randNum.toFixed(2));
+    }
+
+}
+
+class HumanSpaceShip{
+    constructor(hull, firepower, accuracy){
+        this.hull = hull;
+        this.firepower=firepower;
+        this.accuracy=accuracy;
+
+    }
 }
 
 
 
-// hull - between 3 and 6
-function randomHull(min,max){
-    let randNum = Math.random()*(max-min);
+//GAME BEGINS
 
-    if (randNum < min){
-       randNum = Math.round(randNum)+min;
-    }
-
-   return  randNum;
-}
+console.log("Space-battle begins!");
+// //myShip
+let USS_Assembly =  new HumanSpaceShip(20,5,.7);
+console.log("My Stats:\n ",USS_Assembly.accuracy);
 
 
-// firepower - between 2 and 4
-function randomFirePower(min,max){
 
-    let randNum = Math.random()*(max-min);
+console.log(USS_Assembly);
 
-    if (randNum < min){
-       randNum = Math.round(randNum)+min;
-    }
 
-   return  randNum;
-}
 
-// accuracy - between .6and .8
+// let inWar= true;
 
-function randomAccuracy(min,max){
 
-    let randNum = (Math.random()*max);
-    randNum = ((randNum%Math.random())*Math.random());
-    randNum = (Math.random()*randNum);
 
-    if (randNum < min){
-        randNum +=min;
-    }
-    if (randNum >max){
-        randNum = randNum%max;
-    }
+// //let USS_Assembly = new Spaceship(20, 5, .7);
 
-   return  parseFloat(randNum.toFixed(2));
-}
 
-///FUNCTIONS
 
-function attack(){
+
+
+
+
+// //console.log(enemies[0].accuracy,enemies[0].hull, enemies[0].accuracy);
+// console.log("total enemyShips: ",shipCount);
+// //console.log(USS_Assembly.accuracy,USS_Assembly.hull, USS_Assembly.accuracy);
+
+
+
+// let enemy1 = attack.bind(enemies[0]);
+// enemy1();
+
+
+// // battleLoop();
+
+// // function battleLoop (){
+
+// //     for (let i=0 ; i<shipCount; i++){
+// //         enemies[0]= attack.bind(enemies[0]);
+// //         enemies[0];
+       
+// //     }
+
+// // }
+
+
+// function updateShipCount() {
+
     
+// }
+// function peaceRestored(){
+//     console.log("world peace!");
+// }
 
 
-}
+// let enemy1 = stats.bind(enemies[0]);
+// let enemy2 = stats.bind(enemies[1]);
+// let enemy3 = stats.bind(enemies[2]);
+// let enemy4 = stats.bind(enemies[3]);
+// let enemy5 = stats.bind(enemies[4]);
+// let enemy6 = stats.bind(enemies[5]);
 
-function generateSpaceship(){
-    return new Spaceship();
-}
 
 
-
-
-//Testing
-
-let USS_Assembly = new Spaceship(20, 5, .7);
-
-console.log({USS_Assembly});
-
-let enemies = [];
-for (let i = 0; i <6; i++) {
- enemies.push(generateSpaceship());
-}
-
-// console.log("Enemy Ship count: ",{shipCount});
-console.log("array of enemies: ", {enemies});
+// console.log("enemy1:",enemy1());
+// console.log("enemy2:",enemy2());
+// console.log("enemy3:",enemy3());
+// console.log("enemy4:",enemy4());
+// console.log("enemy5:",enemy5());
+// console.log("enemy6:",enemy6());
+// console.log("myship :",mySpaceship());
